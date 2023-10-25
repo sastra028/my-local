8fff33d5b0596f7209d98f32dbe62e05a15e7828
sastra.t <sastra.028@gmail.com>
2023-10-26 00:08:43 +0700

    update table

--- src/components/MyTable2.js
+++ src/components/MyTable2.js
@@ -5,47 +5,39 @@ export class MyTable2 extends Component{
     
 render(){
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-      field: 'firstName',
-      headerName: 'First name',
-      width: 150,
-      editable: true,
+    { 
+      field: 'number', 
+      headerName: 'เลข', 
+      width: 100 
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'age',
-      headerName: 'Age',
+      field: 'price1',
+      headerName: 'ราคา',
+      width: 100,
+      // editable: true,
       type: 'number',
-      width: 110,
-      editable: true,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      field: 'price2',
+      headerName: 'โตด/กลับ',
+      width: 150,
+      // editable: true,
+      type: 'number',
+    },
+    {
+      field: 'action',
+      headerName: 'ดำเนินการ',
+      width: 20,
+      // editable: true,
     },
   ];
   
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, number: '01', price1: 500, price2: 500, action: 'ลบ' },
+    { id: 2, number: '02', price1: 500, price2: 500, action: 'ลบ' },
+    { id: 3, number: '03', price1: 500, price2: 500, action: 'ลบ' },
+    { id: 4, number: '56', price1: 500, price2: 500, action: 'ลบ' },
+    { id: 5, number: '78', price1: 500, price1: 500, action: 'ลบ' },
   ];
   
 
@@ -62,7 +54,7 @@ render(){
           },
         }}
         pageSizeOptions={[5]}
-        checkboxSelection
+        //checkboxSelection
         disableRowSelectionOnClick
       />
     </Box>
