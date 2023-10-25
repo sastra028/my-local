11077ecd5f5d79895c3bda503bc5a506bf5edfb9
sastra.t <sastra.028@gmail.com>
2023-10-26 00:57:33 +0700

    date picker

--- src/components/Input.js
+++ src/components/Input.js
@@ -9,6 +9,9 @@ import Button from '@mui/material/Button';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 export class Input extends Component{
     
 render(){
@@ -23,23 +26,34 @@ render(){
         
         <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2} columns={24}>
-        <Grid xs={24}>
-          <TextField id="outlined-basic" label="งวดที่" variant="outlined" size="small" fullWidth />
+        <Grid container spacing={1} columns={24}>
+          <Grid xs={16}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker />
+            </LocalizationProvider>
+
           </Grid>
-          <Grid xs={24}>
+          <Grid xs={8}>
+            
+          <h3>งวดที่</h3>
+          </Grid>
+          
+        </Grid>
+        <Grid xs={24}>
             <TextField id="outlined-basic" label="ชื่อ" variant="outlined" size="small" fullWidth />
+
           </Grid>
         </Grid>
 
         <Grid container spacing={2} columns={24}>
           <Grid xs={8}>
-          <TextField id="outlined-basic" label="เลข" variant="outlined" fullWidth />
+          <TextField id="outlined-basic" label="เลข" variant="outlined" fullWidth inputMode="numeric" />
           </Grid>
           <Grid xs={8}>
-          <TextField id="outlined-basic" label="ราคา" variant="outlined" fullWidth />
+          <TextField id="outlined-basic" label="ราคา" variant="outlined" fullWidth type="number" />
           </Grid>
           <Grid xs={8}>
-          <TextField id="outlined-basic" label="โตด/กลับ" variant="outlined" fullWidth />
+          <TextField id="outlined-basic" label="โตด/กลับ" variant="outlined" fullWidth type="number"/>
           </Grid>
 
           <Grid xs={15}/>
